0dba1f56217fa183c6b45821e5a8ef7749863823
Aaron Miles <7450155+cooljingle@users.noreply.github.com>
2021-05-17 20:06:18 +0800

    add $(document).ready

--- MemriseTurbo.user.js
+++ MemriseTurbo.user.js
@@ -6,73 +6,74 @@
 // @match          https://www.memrise.com/garden/review/*
 // @match          https://app.memrise.com/course/*/garden/*
 // @match          https://app.memrise.com/garden/review/*
-// @version        0.1.18
+// @version        0.1.19
 // @updateURL      https://github.com/cooljingle/memrise-turbo/raw/master/MemriseTurbo.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-turbo/raw/master/MemriseTurbo.user.js
 // @grant          none
 // ==/UserScript==
 
-console.log("turbo script loaded");
 
-var isComposing = false;
-var cachedEvent;
+$(document).ready(function() {
+    console.log("turbo script loaded");
 
-$('body').on('compositionstart', function() {
-    isComposing = true;
-});
+    var isComposing = false;
+    var cachedEvent;
 
-$('body').on('compositionend', function() {
-    isComposing = false;
-    processInput(cachedEvent);
-});
+    $('body').on('compositionstart', function() {
+        isComposing = true;
+    });
 
-$('body').on('input', function(e) {
-    cachedEvent = e;
-    if ($(e.target).is('input') && !(isComposing)) {
-        processInput(e);
-    }
-});
+    $('body').on('compositionend', function() {
+        isComposing = false;
+        processInput(cachedEvent);
+    });
 
-function processInput(e)
-{
-    try {
-        var g = MEMRISE.garden;
-        var b = g.box;
-        var i = b.$input.input || b.$input;
-        var v = i.val();
-
-        var lastChar = v.slice(-1);
-        var clearBox = false;
-        if(lastChar === "·" || lastChar === "；" || lastChar === ";") {
-            clearBox = true;
-            i.val(v.slice(0, -1));
+    $('body').on('input', function(e) {
+        cachedEvent = e;
+        if ($(e.target).is('input') && !(isComposing)) {
+            processInput(e);
         }
-        if(b.get_score) {
-            var s = b.get_score();
-            if (s === 1)
-                b.check();
+    });
+
+    function processInput(e)
+    {
+        try {
+            var g = MEMRISE.garden;
+            var b = g.box;
+            var i = b.$input.input || b.$input;
+            var v = i.val();
+
+            var lastChar = v.slice(-1);
+            var clearBox = false;
+            if(lastChar === "·" || lastChar === "；" || lastChar === ";") {
+                clearBox = true;
+                i.val(v.slice(0, -1));
+            }
+            if(b.get_score) {
+                var s = b.get_score();
+                if (s === 1)
+                    b.check();
+            }
+            if(clearBox)
+                i.val("");
+        } catch (err) {
+            console.log('error - falling back to default behaviour', err);
         }
-        if(clearBox)
-            i.val("");
-    } catch (err) {
-        console.log('error - falling back to default behaviour', err);
     }
-}
 
+    MEMRISE.garden._events.start.push(() => {
+        NoScoreWhileInputting();
+        NoTimer();
+    });
 
-MEMRISE.garden._events.start.push(() => {
-    NoScoreWhileInputting();
-    NoTimer();
-});
-
-// don't score IME input while still inputting
-function NoScoreWhileInputting() {
-    var turboCount = 0;
-    MEMRISE.garden.session.make_box = (function () {
-        var cached_function = MEMRISE.garden.session.make_box;
-        return function() {
-            var result = cached_function.apply(this, arguments);
-            //choosing mem
+    // don't score IME input while still inputting
+    function NoScoreWhileInputting() {
+        var turboCount = 0;
+        MEMRISE.garden.session.make_box = (function () {
+            var cached_function = MEMRISE.garden.session.make_box;
+            return function() {
+                var result = cached_function.apply(this, arguments);
+                //choosing mem
                 result.next_press = (function () {
                     var cached_function = result.next_press;
                     return function() {
@@ -84,51 +85,52 @@ function NoScoreWhileInputting() {
                         }
                     };
                 }());
-            //multiple choice
-            if(result.select_choice) {
-                result.select_choice = (function () {
-                    var cached_function = result.select_choice;
-                    return function() {
-                        cached_function.apply(this, arguments);
-                        result.next_press();
-                    };
-                }());
-            }
-            //normal checks
-            if(result.check) {
-                result.check = (function () {
-                    var cached_function = result.check;
-                    return function() {
-                        if(!isComposing) {
+                //multiple choice
+                if(result.select_choice) {
+                    result.select_choice = (function () {
+                        var cached_function = result.select_choice;
+                        return function() {
                             cached_function.apply(this, arguments);
-                            if(result.template !== 'copytyping' || result.answeredCorrectly) {
-                                if(turboCount === 0) {
-                                    turboCount++;
-                                    result.next_press(); //quick change screens
-                                    setTimeout(() => turboCount--, 300);
+                            result.next_press();
+                        };
+                    }());
+                }
+                //normal checks
+                if(result.check) {
+                    result.check = (function () {
+                        var cached_function = result.check;
+                        return function() {
+                            if(!isComposing) {
+                                cached_function.apply(this, arguments);
+                                if(result.template !== 'copytyping' || result.answeredCorrectly) {
+                                    if(turboCount === 0) {
+                                        turboCount++;
+                                        result.next_press(); //quick change screens
+                                        setTimeout(() => turboCount--, 300);
+                                    }
                                 }
                             }
-                        }
-                    };
-                }());
-            }
-            return result;
-        };
-    }());
-}
+                        };
+                    }());
+                }
+                return result;
+            };
+        }());
+    }
 
-// always disable timer
-function NoTimer() {
-    MEMRISE.garden.session.make_box = (function () {
-        var cached_function = MEMRISE.garden.session.make_box;
-        return function() {
-            var result = cached_function.apply(this, arguments);
-            result.getTimerLength = () => 0;
-            return result;
-        };
-    }());
-}
+    // always disable timer
+    function NoTimer() {
+        MEMRISE.garden.session.make_box = (function () {
+            var cached_function = MEMRISE.garden.session.make_box;
+            return function() {
+                var result = cached_function.apply(this, arguments);
+                result.getTimerLength = () => 0;
+                return result;
+            };
+        }());
+    }
 
-// always let audio play in full
-MEMRISE.audioPlayer.stop = $.noop;
-MEMRISE.audioPlayer.stopAfter = $.noop;
+    // always let audio play in full
+    MEMRISE.audioPlayer.stop = $.noop;
+    MEMRISE.audioPlayer.stopAfter = $.noop;
+});
